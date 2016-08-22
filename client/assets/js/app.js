3795f3a1055972edcbc6c7551eb8c735e2e9d739
Matthijs Bierman <matthijs@geckotech.nl>
2016-08-22 11:28:12 +0200

    added part to test JS skills

--- client/assets/js/app.js
+++ client/assets/js/app.js
@@ -36,19 +36,49 @@
   }
 
   // note: normally a proper build system with controllers and services in separate directories would be set up
-  angular.module('application').controller('OverviewController', ['$scope', function($scope) {
-    $scope.project = {
-      name:'My Project',
-      start:'2016-06-30T02:00:00.000Z',
-      end:'2016-07-07T02:00:00.000Z',
-      tasks: 32,
-      late: 6,
-      lateConfirmed: 2,
-      done: 24
-    };
+
+  angular.module('application').service('ProjectService', ['$q', function ($q) {
+    // normally this would ben an $http call, here we resolve the promise to simulate a successful $http call
+    this.getProject = function() {
+      return $q(function(resolve) {
+        resolve({
+          name: 'My Project',
+          start: '2016-06-30T02:00:00.000Z',
+          end: '2016-07-07T02:00:00.000Z',
+          tasks: [
+            {name: 'task 1', late: false, confirmed: true, done: true},
+            {name: 'task 2', late: true, confirmed: true, done: true},
+            {name: 'task 3', late: true, confirmed: true, done: true},
+            {name: 'task 4', late: true, confirmed: false, done: false},
+            {name: 'task 5', late: true, confirmed: false, done: false},
+            {name: 'task 6', late: true, confirmed: false, done: false},
+            {name: 'task 7', late: false, confirmed: false, done: false},
+            {name: 'task 8', late: false, confirmed: false, done: false},
+            {name: 'task 9', late: false, confirmed: false, done: false},
+            {name: 'task 10', late: false, confirmed: false, done: false}
+          ]
+        });
+      });
+    }
+  }]);
+
+  angular.module('application').controller('OverviewController', ['$scope', 'ProjectService', function($scope, ProjectService) {
+    ProjectService.getProject().then(function(project) {
+      // TODO calculate values in project
+      $scope.project = {
+        name: project.name,
+        start: project.start,
+        end: project.end,
+        tasks: /*TODO*/ 0, // total nr of tasks
+        late: /*TODO*/ 0, // tasks that are late, but not confirmed
+        lateConfirmed: /*TODO*/ 0, // tasks that are late *and* confirmed
+        done: /*TODO*/ 0 // tasks that are done
+      };
+    });
   }]);
 
   angular.module('application').controller('VisualizationController', ['$scope', function($scope) {
+    // historic data for the project, recorded how many tasks in total and how many were late or lateConfirmed
     $scope.data = [
       {day:0,tasks:30, late:0, lateConfirmed:0},
       {day:1,tasks:30, late:0, lateConfirmed:0},
@@ -69,5 +99,4 @@
       {day:16,tasks:32, late:0, lateConfirmed:2}
     ];
   }]);
-
 })();
